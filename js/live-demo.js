26778d794efc60ef27182d1ff9cfd3263d6780ec
varshalik <varshalikumar@gmail.com>
2017-06-05 03:01:15 -0400

    Update live-demo.js

--- js/live-demo.js
+++ js/live-demo.js
@@ -199,7 +199,7 @@ function handleFiles(files) {
 	// Check for the various File API support.
 	if (window.FileReader) {
 		// FileReader are supported.
-		getAsText(files[0]);
+		//getAsText(files[0]);
 	} else {
 		alert('FileReader are not supported in this browser.');
 	}
